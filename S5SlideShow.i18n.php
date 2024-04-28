30db790419065fc53a68cb23ef4e980482d5f90d
AudioSlav3 <117546907+AudioSlav3@users.noreply.github.com>
2024-04-28 11:19:54 -0500

    Update S5SlideShow.i18n.php
    Updated to support PHP 8.1

--- S5SlideShow.i18n.php
+++ S5SlideShow.i18n.php
@@ -1,26 +1,25 @@
 <?php
 // Internationalization file for S5SlideShow Extension
 
-$magicWords = array();
+$magicWords = [
+  'en' => [
+    's5slideshow' => [1, 'S5SLIDESHOW'],
+  ],
+];
 
-$magicWords['en'] = array(
-    's5slideshow' => array('1', 'S5SLIDESHOW'),
-);
-
-$messages = array();
-
-$messages['en'] = array(
+$messages = [
+  'en' => [
     's5slide-header-title' => 'Title',
     's5slide-header-subtitle' => 'Subtitle',
     's5slide-header-footer' => 'Footer',
     's5slide-header-subfooter' => 'Subfooter',
     's5slide-header-author' => 'Author',
-);
-
-$messages['ru'] = array(
+  ],
+  'ru' => [
     's5slide-header-title' => 'Заголовок',
     's5slide-header-subtitle' => 'Подзаголовок',
     's5slide-header-footer' => 'Нижний колонтитул',
     's5slide-header-subfooter' => 'Дополнительный нижний колонтитул',
     's5slide-header-author' => 'Автор',
-);
+  ],
+];
